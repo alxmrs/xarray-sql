15b573529896d5d99ac768a93663db08b1cb7901
Alex Merose <al@merose.com>
2023-11-26 21:26:10 +1300

    POC: Execution via an `xarray.Dataset` Table. (#1)
    This is a proof of concept for Qarray. Here, I make use of sqlglot's
    built-in SQL executor by making a custom Xarray table. The main function
    of this table is to translate a multi-dimensional array into a flat
    table index.
    
    This is only tested with a simple select-and-limit. More tests are
    needed.

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ setuptools.setup(
     name='qarray',
     version='0.0.0',
     license='Apache 2.0',
-    install_requires=[],
+    install_requires=['xarray', 'sqlglot'],
     extras_require={
         'dev': ['pyink'],
     },
-    python_requires='>=3',
+    python_requires='>=3.9',
 )
