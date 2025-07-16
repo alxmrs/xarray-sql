fd6fe0ecf4d6b49f837b173090b15796f5a9acf8
Alex Merose <al@merose.com>
2025-07-16 11:26:40 -0700

    Properly set up the rust project (with dummy table provide, can compile with `uv build`.

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,11 @@
 use pyo3::prelude::*;
+use crate::table_provider::ZarrTableProvider;
 
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
-}
+pub(crate) mod table_provider;
 
-/// A Python module implemented in Rust.
+
 #[pymodule]
 fn zarrquet(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_class::<ZarrTableProvider>()?;
     Ok(())
 }
