8a331827ab19bf4eb4b1aa1c9031c16641467e1b
Kyle Barron <kyle@developmentseed.org>
2025-07-24 13:40:42 +0200

    rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,7 @@
-use pyo3::prelude::*;
 use crate::table_provider::ZarrTableProvider;
+use pyo3::prelude::*;
 
 pub mod table_provider;
-
 
 #[pymodule]
 fn zarrquet(m: &Bound<'_, PyModule>) -> PyResult<()> {
